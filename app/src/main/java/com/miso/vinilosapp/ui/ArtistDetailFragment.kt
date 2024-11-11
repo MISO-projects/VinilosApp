6bfeda34f38940aa91b1ea45c5b2d6490205c224
Alejandro Peña <ja.penat1@uniandes.edu.co>
2024-11-10 22:52:01 -0500

    [HU04] Add artist detail ui design

---
+++ app/src/main/java/com/miso/vinilosapp/ui/ArtistDetailFragment.kt
@@ -0,0 +1,40 @@
+package com.miso.vinilosapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.miso.vinilosapp.databinding.FragmentArtistDetailBinding
+
+class ArtistDetailFragment : Fragment() {
+    private var _binding: FragmentArtistDetailBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentArtistDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backBtnArtist.setOnClickListener{
+            findNavController().navigateUp()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
