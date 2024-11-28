0f4747318fd663a617a84b3162904602f9db45f2
jhalep-miso <ja.penat1@uniandes.edu.co>
2024-11-27 22:27:47 -0500

    Feature/HU07 add album (#71)
    * [HU07] Add create album ui design
    
    * [HU07] Add call to rest api in album creation
    
    * [HU07] Add validations
    
    * [HU07] Update Home to also refresh albums list
    
    * [HU07] Fix broken tests
    
    * [HU07] Add unit tests

--- app/src/main/java/com/miso/vinilosapp/data/repositories/AlbumRepository.kt
+++ app/src/main/java/com/miso/vinilosapp/data/repositories/AlbumRepository.kt
@@ -1,26 +1,34 @@
 package com.miso.vinilosapp.data.repositories
 
+import android.util.Log
 import com.miso.vinilosapp.data.cache.AlbumsCacheManager
+import com.miso.vinilosapp.data.database.daos.AlbumDao
 import com.miso.vinilosapp.data.models.Album
+import com.miso.vinilosapp.data.models.AlbumRequest
 import com.miso.vinilosapp.data.repositories.network.ApiService
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 
 class AlbumRepository(
     private val albumsCacheManager: AlbumsCacheManager,
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val albumDao: AlbumDao? = null
 ) {
 
-    suspend fun getAlbums(): List<Album> = withContext(Dispatchers.IO) {
-        albumsCacheManager.getAlbums().ifEmpty {
+    suspend fun getAlbums(): Flow<List<Album>> {
+        return flow {
+            albumDao?.getAllAlbums()?.let { emit(it) }
             try {
                 val albumsFromApi = apiService.getAlbums()
-                albumsCacheManager.saveAlbums(albumsFromApi)
-                albumsFromApi
+                albumDao?.insertAll(albumsFromApi)
+                emit(albumsFromApi)
             } catch (e: Exception) {
-                throw RuntimeException("Error fetching albums from network", e)
+                Log.e("Repository", "Error syncing data", e)
             }
-        }
+        }.flowOn(Dispatchers.IO)
     }
 
     suspend fun getAlbumById(id: Int): Album = withContext(Dispatchers.IO) {
@@ -28,6 +36,14 @@ class AlbumRepository(
             apiService.getAlbumById(id)
         } catch (e: Exception) {
             throw RuntimeException("Error fetching album with ID $id from network", e)
+        }
+    }
+
+    suspend fun addAlbum(album: AlbumRequest): Album = withContext(Dispatchers.IO) {
+        try {
+            apiService.addAlbum(album)
+        } catch (e: Exception) {
+            throw RuntimeException("Error adding album to network", e)
         }
     }
 }
